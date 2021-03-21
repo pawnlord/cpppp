fe89d2be9e0228647b0c7e3a72e7192dc842b942
pawnlord <scruffduff321@gmail.com>
2021-03-20 23:33:09 -0400

    Basic function down

--- cpper.py
+++ cpper.py
@@ -1,8 +1,23 @@
-a = ""
+lines = []
 while True:
     try:
-        a = input("")
+        lines.append(input())
     except EOFError:
         print("eof")
         break
-    print(a)
+purged_lines = []
+purging_lines = True
+last_line = ""
+for s in lines:
+    if not purging_lines:
+        purged_lines.append(s)
+    if "error" in s and purging_lines:
+        purging_lines = False
+        purged_lines.append(last_line)
+        purged_lines.append(s)
+    if "note" in s and not purging_lines:
+        purging_lines = True
+        purged_lines = purged_lines[:-1]
+    last_line = s
+for s in purged_lines:
+    print(s)
